86df5d39e425cf6f202526937f64915f9f81c055
shibaofeng <skiloop@gmail.com>
2018-08-15 18:08:24 +0800

    add build files

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+
+
+FROM aliyunfc/aliyun-fc:nodejs8
+
+# ref: https://chromium.googlesource.com/chromium/src.git/+refs
+ARG VERSION
+ENV VERSION ${VERSION:-master}
+
+LABEL maintainer="skiloop <skiloop@gmail.com>"
+LABEL chromium="${VERSION}"
+
+WORKDIR /
+
+ADD build.sh /
+ADD .gclient /build/chromium/
+
+RUN sh /build.sh
+
+EXPOSE 9222
+
+ENTRYPOINT [ \
+  "/bin/headless-chromium", \
+  "--disable-dev-shm-usage", \
+  "--disable-gpu", \
+  "--no-sandbox", \
+  "--hide-scrollbars", \
+  "--remote-debugging-address=0.0.0.0", \
+  "--remote-debugging-port=9222" \
+  ]
---
+++ build.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+set -e
+
+BUILD_BASE=$(pwd)
+VERSION=${VERSION:-master}
+
+printf "LANG=en_US.utf-8\nLC_ALL=en_US.utf-8" >> /etc/environment
+
+# install dependencies
+apt-get install -y lsb-release sudo
+
+mkdir -p build/chromium
+
+cd build
+
+# install dept_tools
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+
+export PATH="/opt/gtk/bin:$PATH:$BUILD_BASE/build/depot_tools"
+
+cd chromium
+
+# fetch chromium source code
+# ref: https://www.chromium.org/developers/how-tos/get-the-code/working-with-release-branches
+git clone https://chromium.googlesource.com/chromium/src.git
+
+(
+  cd src
+
+  # Do a pull because there are usually revisions pushed while we're cloning
+  git pull
+
+  # checkout the release tag
+  git checkout -b build "$VERSION"
+)
+
+# Checkout all the submodules at their branch DEPS revisions
+gclient sync --with_branch_heads --jobs 16
+
+cd src
+
+# install dependencies
+build/install-build-deps.sh --no-arm
+
+# specify build flags
+mkdir -p out/Headless && \
+  echo 'import("//build/args/headless.gn")' > out/Headless/args.gn && \
+  echo 'is_debug = false' >> out/Headless/args.gn && \
+  echo 'symbol_level = 0' >> out/Headless/args.gn && \
+  echo 'is_component_build = false' >> out/Headless/args.gn && \
+  echo 'remove_webcore_debug_symbols = true' >> out/Headless/args.gn && \
+  echo 'enable_nacl = false' >> out/Headless/args.gn && \
+  gn gen out/Headless
+
+# build chromium headless shell
+ninja -C out/Headless headless_shell
+
+cp out/Headless/headless_shell "$BUILD_BASE/bin/headless-chromium-unstripped"
+
+cd "$BUILD_BASE"
+
+# strip symbols
+strip -o "$BUILD_BASE/bin/headless-chromium" build/chromium/src/out/Headless/headless_shell
